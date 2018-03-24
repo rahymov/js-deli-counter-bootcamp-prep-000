1a93d73a9795d483986ecd89648369bbc6a05975
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-24 16:57:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var katzDeli = [];
+function takeANumber(katzDeli, person){
+  
+  katzDeli.push(`${person}`);
+  return(`Welcome, ${person}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeli){
+  var i = 0;
+  while(i < katzDeli.length){
+    i++;
+  }
+  if(katzDeli.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return(`Currently serving ${katzDeli.shift()}.`);
+  }
+}
+
+function currentLine(line){
+  
+}
